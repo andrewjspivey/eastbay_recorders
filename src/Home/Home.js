65100d803cbfd097a333d0de9988399ea508af97
andrewjspivey <12spivey@gmail.com>
2021-11-17 14:23:24 -0800

    adds Recently at ebr and gear intro to home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
 import LandingJumbo from "../LandingJumbo/LandingJumbo";
 import Intro from "../Intro/Intro";
+import Recently from "../Recently/Recently";
+import GearIntro from "../GearIntro/GearIntro";
 
 const Home = () => {
   return (
     <div>
       <LandingJumbo />
       <Intro />
+      <Recently />
+      <GearIntro />
     </div>
   );
 };
