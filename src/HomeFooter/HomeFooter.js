1dcdf43c7c6a6878c16b0f2fabef2f869fcdc0fa
andrewjspivey <12spivey@gmail.com>
2021-11-17 18:34:05 -0800

    adds footer with links, links need styling

--- src/HomeFooter/HomeFooter.js
+++ src/HomeFooter/HomeFooter.js
@@ -1,38 +1,39 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import ebr_record_logo from "../assets/ebr_record_logo.png";
+import { Link } from "react-router-dom";
 
 const Footer = styled.div`
   width: 100%;
   height: 30rem;
   background-color: black;
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
   flex-direction: column;
 `;
-const Social = styled.div``;
+const Social = styled.div`
+  width: 25%;
+  height: 5rem;
+  display: flex;
+  justify-content: space-around;
+`;
 
 const HomeFooter = () => {
-  const [links, setLinks] = useState({ facebook: "", instagram: "" });
-
-  useEffect(() => {
-    const facebookLink = "https://www.facebook.com/EastBayRecorders/";
-    const instagramLink = "https://www.instagram.com/eastbayrecorders/";
-    setLinks({ facebook: facebookLink, instagram: instagramLink });
-  }, []);
-
   return (
     <Footer>
       <div>
         <img src={ebr_record_logo} alt="" />
       </div>
-      <Social></Social>
-      <div>
-        <a href="https://www.facebook.com/EastBayRecorders/">FACEBOOK</a>
+      <Social>
+        <a href="https://www.facebook.com/EastBayRecorders/">
+          <h4>FACEBOOK</h4>
+        </a>
 
-        <a href="https://www.instagram.com/eastbayrecorders/">INSTAGRAM</a>
-      </div>
+        <a href="https://www.instagram.com/eastbayrecorders/">
+          <h4>INSTAGRAM</h4>
+        </a>
+      </Social>
     </Footer>
   );
 };
