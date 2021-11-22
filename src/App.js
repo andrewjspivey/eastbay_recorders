b18122865f5887c903ffb8d36a923f773b32d912
andrewjspivey <12spivey@gmail.com>
2021-11-22 14:31:26 -0800

    adds dynamic routing and data for producerspage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import Home from "./Home/Home";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ProducerPage from "./Producer/ProducerPage";
+import { producers } from "./data/producerData";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          {/* <Route exact path="/search">
-            <SearchComponent searchAnime={searchAnime} />
-            <Search searchContent={searchContent} />
-          </Route>
-          <Route path="/Watchlist">
-            <SearchComponent searchAnime={searchAnime} />
-            <Watchlist />
-          </Route>
-          <Route exact path="/categories">
-            <SearchComponent searchAnime={searchAnime} />
-            <Category />
-          </Route>
-          <Route exact path="/anime/:id">
-            <AnimeDetails />
-          </Route> */}
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/producer/:id" element={<ProducerPage />} />
+          {/* <Route exact path="/about" element={<About />} />
+
+          <Route exact path="/gear" element={<Gear/>} />
+          <Route exact path="/scene" element={<Scene />} />
+          <Route exact path="/blog/"> 
+          */}
         </Routes>
       </Router>
     </div>
---
+++ src/Producer/ProducerPage.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { producers } from "../data/producerData";
+
+const ProducerPage = (props) => {
+  const [producer, setProducer] = useState({});
+  const { id } = useParams();
+  useEffect(() => {
+    const producerInfo = producers.find((p) => p.id === id);
+    setProducer(producerInfo);
+  }, []);
+  return (
+    <>
+      {producer ? (
+        <div style={{ width: "500px", height: "500px" }}>
+          <div style={{ width: "80%", height: "100%" }}>
+            <h1>{producer.name}</h1>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1>Undefined Route</h1>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ProducerPage;
