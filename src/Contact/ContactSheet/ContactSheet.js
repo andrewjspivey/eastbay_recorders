7b40c2c6a12d7fdf9485041e2bea316cb56e7559
andrewjspivey <12spivey@gmail.com>
2023-04-12 15:26:06 -0700

    adds new contact sheet

--- src/Contact/ContactSheet/ContactSheet.js
+++ src/Contact/ContactSheet/ContactSheet.js
@@ -5,85 +5,23 @@ const ContactBody = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-evenly;
-  @media (max-width: 1024px) {
-  }
   @media (max-width: 768px) {
-  }
-  @media (max-width: 481px) {
     flex-flow: column;
     align-items: center;
   }
 `;
 
-const ContactWrapper = styled.form`
-  width: 40%;
-  display: flex;
-  flex-flow: column;
-  align-items: center;
-  @media (max-width: 1024px) {
-  }
+const ContactFormWrapper = styled.div`
+  width: 45%;
+  margin-top: 1rem;
   @media (max-width: 768px) {
+    width: 90%;
   }
-  @media (max-width: 481px) {
-    width: 80%;
-  }
-`;
-
-const ContactGroup = styled.div`
-  margin-top: 2rem;
-`;
-
-const NameInputGroup = styled.input`
-  width: 98%;
-  font-weight: bold;
-  height: 30px;
-  margin-top: 1rem;
-  margin-bottom: 1rem;
-  font-family: "Gotham";
-  @media (max-width: 1024px) {
-  }
-  @media (max-width: 768px) {
-  }
-  @media (max-width: 481px) {
-    margin-top: 0;
-  }
-`;
-
-const CategoryGroup = styled.select`
-  width: 98%;
-  font-weight: bold;
-  color: #7b7b7b;
-  height: 35px;
-  margin-top: 1rem;
-  margin-bottom: 1rem;
-  font-family: "Gotham";
-`;
-
-const MessageGroup = styled.textarea`
-  width: 98%;
-  font-weight: bold;
-  height: 60px;
-  margin-top: 1rem;
-  margin-bottom: 1rem;
-  font-family: "Gotham";
-`;
-
-const ContactButton = styled.button`
-  padding: 10px 30px;
-  background-color: #dd3233;
-  border: none;
-  cursor: pointer;
-  color: white;
-  font-weight: bold;
 `;
 
 const LocationBody = styled.div`
   width: 40%;
-  @media (max-width: 1024px) {
-  }
   @media (max-width: 768px) {
-  }
-  @media (max-width: 481px) {
     width: 80%;
   }
 `;
@@ -98,12 +36,12 @@ const LocationText = styled.div`
   }
   @media (max-width: 768px) {
     font-size: 8px;
-  }
-  @media (max-width: 481px) {
     display: flex;
     flex-flow: column;
     align-items: center;
     text-align: center;
+  }
+  @media (max-width: 481px) {
   }
 `;
 
@@ -114,38 +52,28 @@ const Break = styled.div`
 `;
 
 const ContactSheet = () => {
-  const source =
+  const mapSource =
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.7635986112073!2d-122.29513974827702!3d37.819005879652025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80857f298add4a63%3A0xde61e4f266f0263f!2sEast%20Bay%20Recorders!5e0!3m2!1sen!2sus!4v1638905614670!5m2!1sen!2sus";
+  const formSource =
+    "https://812c3618.sibforms.com/serve/MUIEAEuEtib3DDPg4Eef0G1WU5UHCzOb-Knd0R4rl9QIvcvTxT3RkwS2Ceq5lPKbnQD6s0sYpe6mjf-fZugo68Q_yiV7uj8tkPzMqYLe4QsLBqwDnhHavnjsJ-Ui0MVsGx2lMyJfxwUbBwbUFnKUgbxG7j0JEuIatnFpyyTLDXRtQ_QLocNsEyEujtfuB8KJEowTj_F9POrjBq-e";
   return (
     <ContactBody>
-      <ContactWrapper
-        action="https://formsubmit.co/michaelsrosen@comcast.net"
-        method="POST"
-      >
-        <ContactGroup>
-          <NameInputGroup
-            type="name"
-            name="name"
-            placeholder="*FIRST NAME AND LAST NAME"
-          />
-          <NameInputGroup type="email" name="email" placeholder="*EMAIL" />
-          <NameInputGroup
-            type="phone"
-            name="phone"
-            placeholder="*PHONE NUMBER"
-          />
-          <CategoryGroup>
-            <option value="Questions" name="question" defaultValue="selected">
-              *QUESTIONS
-            </option>
-            <option value="Comments" name="comment">
-              *COMMENTS
-            </option>
-          </CategoryGroup>
-          <MessageGroup name="message" placeholder="*MESSAGE"></MessageGroup>
-        </ContactGroup>
-        <ContactButton type="submit">Submit</ContactButton>
-      </ContactWrapper>
+      <ContactFormWrapper>
+        <iframe
+          title="contact-form"
+          width="540"
+          height="500"
+          src={formSource}
+          frameBorder="0"
+          allowFullScreen
+          style={{
+            display: "block",
+            marginLeft: "auto",
+            marginRight: "auto",
+            maxWidth: "100%",
+          }}
+        ></iframe>
+      </ContactFormWrapper>
       <LocationBody>
         <LocationText>
           <h1>
@@ -157,7 +85,7 @@ const ContactSheet = () => {
           <iframe
             className="maps"
             title="google maps"
-            src={source}
+            src={mapSource}
             width="100%"
             height="300"
           />
